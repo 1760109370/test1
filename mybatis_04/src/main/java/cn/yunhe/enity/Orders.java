c4a837369e38d66803f1d2d17589899249a39d55
黄豪 <1760109370@qq.com>
2023-08-17 17:15:09 +0800

    黄豪修改了实体类

--- mybatis_04/src/main/java/cn/yunhe/enity/Orders.java
+++ mybatis_04/src/main/java/cn/yunhe/enity/Orders.java
@@ -6,7 +6,7 @@ public class Orders {
     private  String  ordertime;
     private  double money;
     private  User user;
-    private  String password;
+    private  String name;
 
     public Orders() {
     }
